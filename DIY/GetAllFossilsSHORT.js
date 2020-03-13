c6586d52d8a5e09f2f77f8236051fafe3b8d41df
Ocenting <62109547+Ocenting@users.noreply.github.com>
2020-03-13 15:44:33 -0400

    Add files via upload

---
+++ DIY/GetAllFossilsSHORT.js
@@ -0,0 +1,5 @@
+// Simplified version of getting all fossils. Found by @Magmischief.
+PIXI.game.prodigy.debugMisc.drDino();
+
+// Bookmarklet:
+javascript:PIXI.game.prodigy.debugMisc.drDino()%3Bvoid+0
---
+++ DIY/SetFPS.js
@@ -0,0 +1,5 @@
+// Sets game FPS. Found by @BOOM-BOOM-THE-GREAT.
+PIXI.game.prodigy.debugMisc.SetFPS();
+
+// Bookmarklet:
+javascript:PIXI.game.prodigy.debugMisc.SetFPS()%3Bvoid+0
